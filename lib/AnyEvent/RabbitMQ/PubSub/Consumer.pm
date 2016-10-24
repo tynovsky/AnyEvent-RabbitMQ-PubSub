94b7b520ddd8c42dd677d53b0e678ab7d6c47eb8
Miroslav Tynovsky <tynovsky@avast.com>
2016-10-24 15:46:07 +0200

    added consumer methods ack and reject_and_republish

--- lib/AnyEvent/RabbitMQ/PubSub/Consumer.pm
+++ lib/AnyEvent/RabbitMQ/PubSub/Consumer.pm
@@ -1,5 +1,8 @@
 package AnyEvent::RabbitMQ::PubSub::Consumer;
 use Moose;
+use AnyEvent::RabbitMQ::PubSub;
+use Data::Dumper;
+use Time::HiRes qw(usleep);
 
 use AnyEvent;
 use Promises qw(deferred collect);
@@ -16,9 +19,14 @@ has queue => (
 has routing_key => (
     is => 'ro', isa => 'Str', default => '#'
 );
+has prefetch_count => (
+    is => 'ro', isa => 'Int', default => 5,
+);
 
 sub init {
     my ($self) = @_;
+
+    $self->channel->qos(prefetch_count => $self->prefetch_count);
 
     my $cv = AnyEvent->condvar;
 
@@ -39,12 +47,38 @@ sub consume {
         queue      => $self->queue->{queue},
         no_ack     => 0,
         on_success => sub { $d->resolve() },
-        on_cancel  => sub { $cv->croak("consume cancel: @_") },
-        on_failure => sub { $cv->croak("consume failure: @_") },
+        on_cancel  => sub {AnyEvent::RabbitMQ::PubSub::_report_error($cv, @_)},
+        on_failure => sub {AnyEvent::RabbitMQ::PubSub::_report_error($cv, @_)},
         on_consume => sub { $on_consume->($self, @_) },
     );
 
     return $d->promise
+}
+
+sub reject_and_republish {
+    my ($self, $msg) = @_;
+
+    usleep 10_000; # wait 10 ms before republish to avoid 100 % CPU
+    my $delivery_tag = $msg->{deliver}{method_frame}{delivery_tag};
+
+    $self->channel->reject(delivery_tag => $delivery_tag);
+
+    $msg->{header}{headers}{trials}++;
+    $self->channel->publish(
+        body        => $msg->{body}->{payload},
+        header      => $msg->{header},
+        exchange    => "",
+        routing_key => $self->queue->{queue},
+    );
+}
+
+sub ack {
+    my ($self, $msg) = @_;
+
+    warn "Message to ack not specified" if !defined $msg;
+
+    my $delivery_tag = $msg->{deliver}{method_frame}{delivery_tag};
+    $self->channel->ack(delivery_tag => $delivery_tag);
 }
 
 sub declare_exchange_and_queue {
